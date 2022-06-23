bed1c3a124b9717a54ef1e287a3ee9aadbc3dbf5
l33t-c0d3r-66 <aliraza.arain.12530@gmail.com>
2022-06-23 19:12:51 +0500

    Added Layout and Navbar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout/MainNavigation/MainNavigation.js
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom'
+
+import logo from '../../../assets/soda.png';
+import cssClasses from './MainNavigation.module.css';
+
+function MainNavigation () {
+    return (
+        <nav className={cssClasses.MainNavigation}>
+            <div>
+                <Link to="/">
+                    <img src={logo} alt="Soda Store" className={cssClasses.Logo} /> 
+                </Link>
+                <ul className={cssClasses.NavLinks}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    );
+}
+
+export default MainNavigation;
